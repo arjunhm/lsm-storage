f6e88fb56fd637feea94cb05694088f6427d8371
arjunhm <hm.arjun97@gmail.com>
2024-05-30 00:07:04 -0400

    created binary.go
    Contains wrappers around 'encoding/binary' functions

---
+++ binary.go
@@ -0,0 +1,11 @@
+package lsm
+
+import "encoding/binary"
+
+func Putuint32(buf []byte, val uint32) {
+	binary.LittleEndian.PutUint32(buf, val)
+}
+
+func Getuint32(buf []byte) uint32 {
+	return binary.LittleEndian.Uint32(buf)
+}
